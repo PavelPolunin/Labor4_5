5bc309bb5b139358288e07fe5adcfa411582b062
Pavel <Anya@LAPTOP-K4AVO9IM>
2025-04-13 17:28:33 +0300

    Добавьте файлы проекта.

---
+++ WpfApp4_5/HelperClasses/TypePage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4_5.HelperClasses
+{
+    public enum TypePage
+    {
+        Add,
+        Update
+    }
+}
